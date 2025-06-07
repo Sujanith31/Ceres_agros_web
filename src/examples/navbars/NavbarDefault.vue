01ff4d3e7d0c6787a6b4ec164bb1de017d21e0a0
Sujanith31 <sujanithreddy44@gmail.com>
2025-06-07 15:03:10 +0530

    nav updates

--- src/examples/navbars/NavbarDefault.vue
+++ src/examples/navbars/NavbarDefault.vue
@@ -201,12 +201,12 @@ watch(
                 <div class="col-12 px-4 py-2">
                   <div class="row">
                     <div class="position-relative">
-                      <!-- <RouterLink
+                      <RouterLink
                         :to="{ name: 'aboutUs' }"
                         class="dropdown-item border-radius-md"
                       >
                         <span>Our Story</span>
-                      </RouterLink> -->
+                      </RouterLink>
                       <RouterLink
                         :to="{ name: 'about' }"
                         class="dropdown-item border-radius-md"
